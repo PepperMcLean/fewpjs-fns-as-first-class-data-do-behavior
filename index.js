571ee05e3b6b102bca62f8305cb15bab6be09fef
PepperMcLean <jackmcl206@gmail.com>
2022-01-14 17:56:30 -0600

    commit tree f647a5373bfd34c122c90d3401664e1c25fb0f79

--- index.js
+++ index.js
@@ -10,4 +10,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  const intTime = parseInt(time.split(':'))
+  if (intTime < 12) {
+    return "Good Morning"
+  }
+  else if (intTime >= 12 && intTime <= 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(text) {
+  // let main = document.querySelector('main#main')
+  let greet = document.getElementById('greeting')
+  greet.innerHTML = text
+}
